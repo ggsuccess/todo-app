12ab2abb0819c7ed02caefb00de1d0431325d4de
ggsuccess <ggsuccess@naver.com>
2019-11-15 13:57:08 +0900

    todosView

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '클론 코딩해보기',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '기업 지원하기',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: '알고리즘 풀기',
+      checked: false,
+    },
+  ]);
   return (
     <TodoTemplate>
       <TodoInsert />
-      <TodoList />
+      <TodoList todos={todos} />
     </TodoTemplate>
   );
 };
